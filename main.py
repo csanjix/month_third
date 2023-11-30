f1e7c840ff23c4f3bbd818941dc3bca6f8562b58
csanjixx <akmatbekovsultan@gmail.com>
2023-11-30 20:21:26 -0100

    hw3

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from handlers import (
     start,
     callback,
     chat_actions,
-    registraition
+    registration
 )
 from database import sql_commands
 
@@ -13,9 +13,9 @@ async def on_startup(_):
     db.sql_create_tables()
 
 start.register_start_handlers(dp=dp)
-callback.register_callback_handlers(dp=dp)
+callback.register_call_back_handlers(dp=dp)
 chat_actions.register_chat_actions_handlers(dp=dp)
-registraition.register_registration_handlers(dp=dp)
+registration.register_registration_handlers(dp=dp)
 
 if __name__ == "__main__":
     executor.start_polling(
