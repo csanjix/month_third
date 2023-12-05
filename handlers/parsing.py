5c4d1cf00775e0ea187ac90d893f1eb5d7e825c0
csanjixx <akmatbekovsultan@gmail.com>
2023-12-05 00:22:08 -0100

    hw6

--- handlers/parsing.py
+++ handlers/parsing.py
@@ -1,7 +1,19 @@
 from scraping.scraper import Scraper
 from aiogram import types
+import requests
 
 async def get_gata(message: types.Message):
     url_to_scrape = 'https://www.kinopoisk.ru/?utm_referrer=www.google.com'
     scraper = Scraper(url=url_to_scrape)
     data = scraper.scraper_data()
+
+    try:
+        for index, data in enumerate('result'[:5], start=1):
+            await message.answer(f'Результат скрапинга {index}. {data}')
+
+    except (TypeError, IndexError):
+        print('Результаты скрапинга:')
+        print('result')
+
+    except Exception as e:
+        print(f"Ошибка: {e}")
